9c3004c8793c583f5a451a4c888ff0b571c4d921
jenniferagbeli <agbelij12@gmail.com>
2024-02-17 03:44:43 +1200

    first commit

---
+++ src/components/todo-list/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+function TodoList() {
+  // let todos = ["take fufu", "take ginger"];
+  const [todos, setTodos] = React.useState([]);
+
+  function getTodos() {
+    // Get all todos from local storage and store it.
+    let todos = JSON.parse(localStorage.getItem("TODO_KEY")) || [];
+    // update React State
+    setTodos(todos);
+  }
+
+  React.useEffect(getTodos, []);
+
+    return (
+      <ul> 
+        {todos.map(function(todo, index) {
+          return <li key={index}>{todo}</li>
+        })}
+      </ul>
+    );
+  }
+  
+  export default TodoList;
